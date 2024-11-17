08f338bf90406bd91114ac90ec447f1139413dcd
nexpid <60316309+nexpid@users.noreply.github.com>
2024-11-17 23:29:26 +0100

    feat: delete oldsrc

--- oldsrc/app.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Redirect } from "@nestjs/common";
-
-@Controller()
-export class AppController {
-  @Redirect("https://github.com/nexpid/CloudSync", 301)
-  @Get()
-  redirectToGitHub(): void {}
-}
