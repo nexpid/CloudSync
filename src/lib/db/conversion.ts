4e07bdfb9fb055f591999e4d0df403e40d6e502c
nexpid <me@nexpid.xyz>
2025-07-06 16:45:50 +0200

    fix(package): update format script

--- src/lib/db/conversion.ts
+++ src/lib/db/conversion.ts
@@ -1,7 +1,7 @@
 import { promisify } from "node:util";
 import {
-    brotliCompress as _brotliCompress,
-    brotliDecompress as _brotliDecompress,
+	brotliCompress as _brotliCompress,
+	brotliDecompress as _brotliDecompress,
 } from "node:zlib";
 import { UserData, UserDataSchema } from ".";
 
@@ -11,200 +11,200 @@ const brotliDecompress = promisify(_brotliDecompress);
 const noInvalidChars = /[\n\u0000\u0001]/g;
 
 const stripNoCloudSync = (obj: any) => {
-    if (obj && typeof obj === "object") {
-        if ("__no_cloud_sync" in obj) return undefined;
+	if (obj && typeof obj === "object") {
+		if ("__no_cloud_sync" in obj) return undefined;
 
-        for (const key of Object.keys(obj)) {
-            const val = stripNoCloudSync(obj[key]);
-            if (val === undefined) delete obj[key];
-            else obj[key] = val;
-        }
-    }
-    return obj;
+		for (const key of Object.keys(obj)) {
+			const val = stripNoCloudSync(obj[key]);
+			if (val === undefined) delete obj[key];
+			else obj[key] = val;
+		}
+	}
+	return obj;
 };
 
 export function reconstruct(data: string) {
-    const [version, plugins, themes, installedFonts, customFonts, ...incorrect] = data
-        .split("\n");
-    if (incorrect.length > 1 || version !== "2") return;
+	const [version, plugins, themes, installedFonts, customFonts, ...incorrect] = data
+		.split("\n");
+	if (incorrect.length > 1 || version !== "2") return;
 
-    const dataObj: UserData = {
-        plugins: {},
-        themes: {},
-        fonts: {
-            installed: {},
-            custom: [],
-        },
-    };
+	const dataObj: UserData = {
+		plugins: {},
+		themes: {},
+		fonts: {
+			installed: {},
+			custom: [],
+		},
+	};
 
-    for (const plugin of plugins.split("\u0001")) {
-        const stuff = plugin.split("\u0000");
-        const url = stuff[0];
-        if (!url) continue;
-        let [enabled, storage] = stuff.slice(1);
+	for (const plugin of plugins.split("\u0001")) {
+		const stuff = plugin.split("\u0000");
+		const url = stuff[0];
+		if (!url) continue;
+		let [enabled, storage] = stuff.slice(1);
 
-        if (enabled && enabled !== "1") {
-            storage = enabled;
-            enabled = null;
-        }
+		if (enabled && enabled !== "1") {
+			storage = enabled;
+			enabled = null;
+		}
 
-        try {
-            new URL(url);
-            if (storage) JSON.parse(storage);
-        } catch {
-            continue;
-        }
+		try {
+			new URL(url);
+			if (storage) JSON.parse(storage);
+		} catch {
+			continue;
+		}
 
-        dataObj.plugins[url] = {
-            enabled: enabled === "1",
-            storage,
-        };
-    }
+		dataObj.plugins[url] = {
+			enabled: enabled === "1",
+			storage,
+		};
+	}
 
-    for (const font of customFonts.split("\u0001")) {
-        const [spec, src, enabled] = font.split("\u0000");
-        if (Number.isNaN(Number(spec)) || !src) continue;
+	for (const font of customFonts.split("\u0001")) {
+		const [spec, src, enabled] = font.split("\u0000");
+		if (Number.isNaN(Number(spec)) || !src) continue;
 
-        let dt: any;
-        try {
-            dt = JSON.parse(src);
-        } catch {
-            continue;
-        }
+		let dt: any;
+		try {
+			dt = JSON.parse(src);
+		} catch {
+			continue;
+		}
 
-        dataObj.fonts.custom.push({
-            spec: Number(spec),
-            enabled: enabled === "1",
-            ...dt,
-        });
-    }
+		dataObj.fonts.custom.push({
+			spec: Number(spec),
+			enabled: enabled === "1",
+			...dt,
+		});
+	}
 
-    for (const theme of themes.split("\u0001")) {
-        const [url, enabled] = theme.split("\u0000");
-        if (!url) continue;
+	for (const theme of themes.split("\u0001")) {
+		const [url, enabled] = theme.split("\u0000");
+		if (!url) continue;
 
-        try {
-            new URL(url);
-        } catch {
-            continue;
-        }
+		try {
+			new URL(url);
+		} catch {
+			continue;
+		}
 
-        dataObj.themes[url] = {
-            enabled: enabled === "1",
-        };
-    }
+		dataObj.themes[url] = {
+			enabled: enabled === "1",
+		};
+	}
 
-    for (const font of installedFonts.split("\u0001")) {
-        const [url, enabled] = font.split("\u0000");
-        if (!url) continue;
+	for (const font of installedFonts.split("\u0001")) {
+		const [url, enabled] = font.split("\u0000");
+		if (!url) continue;
 
-        try {
-            new URL(url);
-        } catch {
-            continue;
-        }
+		try {
+			new URL(url);
+		} catch {
+			continue;
+		}
 
-        dataObj.fonts.installed[url] = {
-            enabled: enabled === "1",
-        };
-    }
+		dataObj.fonts.installed[url] = {
+			enabled: enabled === "1",
+		};
+	}
 
-    return dataObj;
+	return dataObj;
 }
 
 export function deconstruct(data: UserData) {
-    const { error } = UserDataSchema.safeParse(data);
-    if (error) throw error;
+	const { error } = UserDataSchema.safeParse(data);
+	if (error) throw error;
 
-    const chunks = new Array<string>();
-    chunks.push("2"); // data version, used for major data structure changes
+	const chunks = new Array<string>();
+	chunks.push("2"); // data version, used for major data structure changes
 
-    const pluginChunks = new Array<string>();
-    const themeChunks = new Array<string>();
-    const installedFontChunks = new Array<string>();
-    const customFontChunks = new Array<string>();
+	const pluginChunks = new Array<string>();
+	const themeChunks = new Array<string>();
+	const installedFontChunks = new Array<string>();
+	const customFontChunks = new Array<string>();
 
-    // PLUGINS
-    for (const url of Object.keys(data.plugins)) {
-        const { enabled, storage } = data.plugins[url];
-        const subChunks = new Array<string>();
+	// PLUGINS
+	for (const url of Object.keys(data.plugins)) {
+		const { enabled, storage } = data.plugins[url];
+		const subChunks = new Array<string>();
 
-        subChunks.push(url.replace(noInvalidChars, ""));
-        if (enabled) subChunks.push("1");
+		subChunks.push(url.replace(noInvalidChars, ""));
+		if (enabled) subChunks.push("1");
 
-        const dt = storage && stripNoCloudSync(JSON.parse(storage));
-        if (dt && Boolean(Object.keys(dt).length)) {
-            subChunks.push(JSON.stringify(dt).replace(noInvalidChars, ""));
-        }
+		const dt = storage && stripNoCloudSync(JSON.parse(storage));
+		if (dt && Boolean(Object.keys(dt).length)) {
+			subChunks.push(JSON.stringify(dt).replace(noInvalidChars, ""));
+		}
 
-        pluginChunks.push(subChunks.join("\u0000"));
-    }
+		pluginChunks.push(subChunks.join("\u0000"));
+	}
 
-    // CUSTOM FONTS
-    for (const font of data.fonts.custom) {
-        const subChunks = new Array<string>();
+	// CUSTOM FONTS
+	for (const font of data.fonts.custom) {
+		const subChunks = new Array<string>();
 
-        const fontData = { ...font };
-        delete fontData.enabled;
-        delete fontData.spec;
+		const fontData = { ...font };
+		delete fontData.enabled;
+		delete fontData.spec;
 
-        subChunks.push(font.spec.toString());
-        subChunks.push(JSON.stringify(fontData));
-        if (font.enabled) subChunks.push("1");
+		subChunks.push(font.spec.toString());
+		subChunks.push(JSON.stringify(fontData));
+		if (font.enabled) subChunks.push("1");
 
-        customFontChunks.push(subChunks.join("\u0000"));
-    }
+		customFontChunks.push(subChunks.join("\u0000"));
+	}
 
-    // THEMES
-    for (const url of Object.keys(data.themes)) {
-        const enabled = data.themes[url].enabled;
-        const subChunks = new Array<string>();
+	// THEMES
+	for (const url of Object.keys(data.themes)) {
+		const enabled = data.themes[url].enabled;
+		const subChunks = new Array<string>();
 
-        subChunks.push(url.replace(noInvalidChars, ""));
-        if (enabled) subChunks.push(enabled ? "1" : "0");
+		subChunks.push(url.replace(noInvalidChars, ""));
+		if (enabled) subChunks.push(enabled ? "1" : "0");
 
-        themeChunks.push(subChunks.join("\u0000"));
-    }
+		themeChunks.push(subChunks.join("\u0000"));
+	}
 
-    // INSTALLED FONTS
-    for (const url of Object.keys(data.fonts.installed)) {
-        const enabled = data.fonts.installed[url].enabled;
-        const subChunks = new Array<string>();
+	// INSTALLED FONTS
+	for (const url of Object.keys(data.fonts.installed)) {
+		const enabled = data.fonts.installed[url].enabled;
+		const subChunks = new Array<string>();
 
-        subChunks.push(url.replace(noInvalidChars, ""));
-        if (enabled) subChunks.push("1");
+		subChunks.push(url.replace(noInvalidChars, ""));
+		if (enabled) subChunks.push("1");
 
-        installedFontChunks.push(subChunks.join("\u0000"));
-    }
+		installedFontChunks.push(subChunks.join("\u0000"));
+	}
 
-    chunks.push(
-        pluginChunks.join("\u0001"),
-        themeChunks.join("\u0001"),
-        installedFontChunks.join("\u0001"),
-        customFontChunks.join("\u0001"),
-    );
+	chunks.push(
+		pluginChunks.join("\u0001"),
+		themeChunks.join("\u0001"),
+		installedFontChunks.join("\u0001"),
+		customFontChunks.join("\u0001"),
+	);
 
-    return chunks.join("\n");
+	return chunks.join("\n");
 }
 
 export async function compressData(data: UserData) {
-    return (await brotliCompress(deconstruct(data))).toString("base64");
+	return (await brotliCompress(deconstruct(data))).toString("base64");
 }
 
 export function decompressData(data: string, decompOnly: true): Promise<string>;
 export function decompressData(
-    data: string,
-    decompOnly?: false,
+	data: string,
+	decompOnly?: false,
 ): Promise<UserData>;
 
 export async function decompressData(
-    data: string,
-    decompOnly: boolean = false,
+	data: string,
+	decompOnly: boolean = false,
 ) {
-    const decomp = (
-        await brotliDecompress(Buffer.from(data, "base64"))
-    ).toString();
+	const decomp = (
+		await brotliDecompress(Buffer.from(data, "base64"))
+	).toString();
 
-    if (decompOnly === true) return decomp;
-    else return reconstruct(decomp);
+	if (decompOnly === true) return decomp;
+	else return reconstruct(decomp);
 }
--- src/silly/index.ts
+++ src/silly/index.ts
@@ -5,7 +5,7 @@ import { env } from "src/lib/env";
 import { SillyService } from "./service";
 
 let initiated = false,
-    doingSilly = false;
+	doingSilly = false;
 
 const description = `Syncs your Revenge plugins, themes and fonts to the cloud!
 « https://github.com/nexpid/CloudSync »
@@ -13,117 +13,117 @@ const description = `Syncs your Revenge plugins, themes and fonts to the cloud!
 « https://discord.gg/ddcQf3s2Uq »`;
 
 export async function runSilly() {
-    if (!env.CLIENT_TOKEN) return console.debug({ silly: { enabled: false } });
+	if (!env.CLIENT_TOKEN) return console.debug({ silly: { enabled: false } });
 
-    if (doingSilly) return console.warn({ silly: { busy: true } });
+	if (doingSilly) return console.warn({ silly: { busy: true } });
 
-    doingSilly = true;
-    if (!initiated) {
-        try {
-            await initWasm(resvgWasm);
-            initiated = true;
-        } catch (error) {
-            console.error({ silly: { resvg: false, error } });
-            return (doingSilly = false);
-        }
-    }
+	doingSilly = true;
+	if (!initiated) {
+		try {
+			await initWasm(resvgWasm);
+			initiated = true;
+		} catch (error) {
+			console.error({ silly: { resvg: false, error } });
+			return (doingSilly = false);
+		}
+	}
 
-    const colors = SillyService.getRandomColors();
+	const colors = SillyService.getRandomColors();
 
-    const icon = SillyService.getIcon()
-        .replace(/#FF0000/g, colors.bg)
-        .replace(/#00FF00/g, colors.cloud)
-        .replace(/#0000FF/g, colors.cloudOutline);
-    const banner = SillyService.getBanner()
-        .replace(/#FF0000/g, colors.bg)
-        .replace(/#00FF00/g, colors.cloud)
-        .replace(/#0000FF/g, colors.cloudOutline);
+	const icon = SillyService.getIcon()
+		.replace(/#FF0000/g, colors.bg)
+		.replace(/#00FF00/g, colors.cloud)
+		.replace(/#0000FF/g, colors.cloudOutline);
+	const banner = SillyService.getBanner()
+		.replace(/#FF0000/g, colors.bg)
+		.replace(/#00FF00/g, colors.cloud)
+		.replace(/#0000FF/g, colors.cloudOutline);
 
-    const iconSvg = "data:image/png;base64,"
-        + Buffer.from(
-            new Resvg(icon, {
-                fitTo: { mode: "width", value: 512 },
-                font: { loadSystemFonts: false },
-                shapeRendering: 2,
-            })
-                .render()
-                .asPng(),
-        ).toString("base64");
-    const bannerSvg = "data:image/png;base64,"
-        + Buffer.from(
-            new Resvg(banner, {
-                fitTo: { mode: "width", value: 680 },
-                font: { loadSystemFonts: false },
-                shapeRendering: 2,
-            })
-                .render()
-                .asPng(),
-        ).toString("base64");
-    const ftpe = SillyService.getFtpe(colors.cloud, colors.bg);
+	const iconSvg = "data:image/png;base64,"
+		+ Buffer.from(
+			new Resvg(icon, {
+				fitTo: { mode: "width", value: 512 },
+				font: { loadSystemFonts: false },
+				shapeRendering: 2,
+			})
+				.render()
+				.asPng(),
+		).toString("base64");
+	const bannerSvg = "data:image/png;base64,"
+		+ Buffer.from(
+			new Resvg(banner, {
+				fitTo: { mode: "width", value: 680 },
+				font: { loadSystemFonts: false },
+				shapeRendering: 2,
+			})
+				.render()
+				.asPng(),
+		).toString("base64");
+	const ftpe = SillyService.getFtpe(colors.cloud, colors.bg);
 
-    // "Bot " is included in the token
-    const id = env.CLIENT_ID,
-        token = env.CLIENT_TOKEN;
-    const changedIconReq = await fetch(RouteBases.api + `/applications/${id}`, {
-        method: "PATCH",
-        headers: {
-            "content-type": "application/json",
-            authorization: token,
-        },
-        body: JSON.stringify({
-            icon: iconSvg,
-            description: `${description}${ftpe}`,
-        }),
-    });
-    const changedIcon = await changedIconReq.json<any>();
+	// "Bot " is included in the token
+	const id = env.CLIENT_ID,
+		token = env.CLIENT_TOKEN;
+	const changedIconReq = await fetch(RouteBases.api + `/applications/${id}`, {
+		method: "PATCH",
+		headers: {
+			"content-type": "application/json",
+			authorization: token,
+		},
+		body: JSON.stringify({
+			icon: iconSvg,
+			description: `${description}${ftpe}`,
+		}),
+	});
+	const changedIcon = await changedIconReq.json<any>();
 
-    const changedBannerReq = await fetch(RouteBases.api + Routes.user(), {
-        method: "PATCH",
-        headers: {
-            "content-type": "application/json",
-            authorization: token,
-        },
-        body: JSON.stringify({
-            banner: bannerSvg,
-        }),
-    });
-    const changedBanner = await changedBannerReq.json<any>();
+	const changedBannerReq = await fetch(RouteBases.api + Routes.user(), {
+		method: "PATCH",
+		headers: {
+			"content-type": "application/json",
+			authorization: token,
+		},
+		body: JSON.stringify({
+			banner: bannerSvg,
+		}),
+	});
+	const changedBanner = await changedBannerReq.json<any>();
 
-    doingSilly = false;
+	doingSilly = false;
 
-    if (!changedIcon?.id || !changedBanner?.id) {
-        return console.error({
-            silly: {
-                logs: [changedIcon, changedBanner].filter((x) => !x.id),
-                ratelimits: Object.fromEntries(
-                    (
-                        [
-                            ["icon", [...changedIconReq.headers.entries()]],
-                            ["banner", [...changedBannerReq.headers.entries()]],
-                        ] as [string, [string, string][]][]
-                    ).map(([key, values]) => [
-                        key,
-                        Object.fromEntries(
-                            values.filter(([header]) => header.toLowerCase().includes("x-rate")),
-                        ),
-                    ]),
-                ),
-                success: false,
-            },
-        });
-    } else {
-        return console.debug({
-            silly: {
-                colors,
-                banner: RouteBases.cdn
-                    + CDNRoutes.userBanner(id, changedBanner.banner, ImageFormat.PNG),
-                avatar: RouteBases.cdn
-                    + CDNRoutes.userAvatar(id, changedBanner.avatar, ImageFormat.PNG),
-                icon: RouteBases.cdn
-                    + CDNRoutes.applicationIcon(id, changedIcon.icon, ImageFormat.PNG),
-                ftpe,
-                success: true,
-            },
-        });
-    }
+	if (!changedIcon?.id || !changedBanner?.id) {
+		return console.error({
+			silly: {
+				logs: [changedIcon, changedBanner].filter((x) => !x.id),
+				ratelimits: Object.fromEntries(
+					(
+						[
+							["icon", [...changedIconReq.headers.entries()]],
+							["banner", [...changedBannerReq.headers.entries()]],
+						] as [string, [string, string][]][]
+					).map(([key, values]) => [
+						key,
+						Object.fromEntries(
+							values.filter(([header]) => header.toLowerCase().includes("x-rate")),
+						),
+					]),
+				),
+				success: false,
+			},
+		});
+	} else {
+		return console.debug({
+			silly: {
+				colors,
+				banner: RouteBases.cdn
+					+ CDNRoutes.userBanner(id, changedBanner.banner, ImageFormat.PNG),
+				avatar: RouteBases.cdn
+					+ CDNRoutes.userAvatar(id, changedBanner.avatar, ImageFormat.PNG),
+				icon: RouteBases.cdn
+					+ CDNRoutes.applicationIcon(id, changedIcon.icon, ImageFormat.PNG),
+				ftpe,
+				success: true,
+			},
+		});
+	}
 }
