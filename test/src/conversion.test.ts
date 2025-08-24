745e9d2fceca909d8478d625494e3cd55692e4df
nexpid <me@nexpid.xyz>
2025-08-24 20:54:32 +0200

    fix(test): remove duplicate code

--- test/src/conversion.test.ts
+++ test/src/conversion.test.ts
@@ -1,72 +1,8 @@
 import { exists, mkdir, rm } from "node:fs/promises";
 import { brotliCompressSync, brotliDecompressSync } from "node:zlib";
 
-import type { UserData } from "../../src/lib/db";
 import { deconstruct, reconstruct } from "../../src/lib/db/conversion";
-
-const basePluginRoot = "https://bn-plugins.github.io/vd-proxy/user.github.io/plugins/";
-const baseThemeRoot = "https://raw.githubusercontent.com/user/themes/main/";
-const baseFontRoot = "https://raw.githubusercontent.com/user/fonts/main/";
-
-const mockData: UserData = {
-	plugins: Object.fromEntries(
-		new Array(60).fill(0).map(() => [
-			`${basePluginRoot}${crypto.randomUUID()}`,
-			{
-				enabled: Math.random() < 0.5,
-				storage: JSON.stringify(
-					Object.fromEntries(
-						Object.entries({
-							someValue: 0,
-							thing: true,
-							someOtherProperty: false,
-							extremelyLongPropertyName: {
-								enabled: true,
-								bumpscosity: Math.random(),
-							},
-							yetAnotherProperty: [Math.random(), false, "Hi!"],
-						}).filter(() => Math.random() < 0.5),
-					),
-				),
-			},
-		]),
-	),
-	themes: Object.fromEntries(
-		new Array(10).fill(0).map(() => [
-			`${baseThemeRoot}${crypto.randomUUID()}.json`,
-			{
-				enabled: Math.random() < 0.5,
-			},
-		]),
-	),
-	fonts: {
-		installed: Object.fromEntries(
-			new Array(10)
-				.fill(0)
-				.map(() => [
-					`${baseFontRoot}${crypto.randomUUID()}.json`,
-					{ enabled: Math.random() < 0.5 },
-				]),
-		),
-		custom: new Array(1).fill(0).map(() => ({
-			spec: 2,
-			name: crypto.randomUUID(),
-			previewText: new Array(Math.floor(Math.random() * 5) + 1)
-				.fill(0)
-				.map(() => crypto.randomUUID())
-				.join(""),
-			main: Object.fromEntries(
-				new Array(12)
-					.fill(0)
-					.map(() => [
-						crypto.randomUUID(),
-						`${baseFontRoot}/${crypto.randomUUID()}.ttf`,
-					]),
-			),
-			enabled: Math.random() < 0.5,
-		})),
-	},
-};
+import { mockData } from "./makedata.test";
 
 const byteSizer = (len: number) => {
 	if (len < 1e3) return `${len} B`;
--- test/src/makedata.test.ts
+++ test/src/makedata.test.ts
@@ -6,29 +6,31 @@ const basePluginRoot = "https://bn-plugins.github.io/vd-proxy/user.github.io/plu
 const baseThemeRoot = "https://raw.githubusercontent.com/user/themes/main/";
 const baseFontRoot = "https://raw.githubusercontent.com/user/fonts/main/";
 
-const mockData: UserData = {
+export const mockData: UserData = {
 	plugins: Object.fromEntries(
-		new Array(5).fill(0).map(() => [
+		new Array(60).fill(0).map(() => [
 			`${basePluginRoot}${crypto.randomUUID()}`,
 			{
 				enabled: Math.random() < 0.5,
-				storage: JSON.stringify(Object.fromEntries(
-					Object.entries({
-						someValue: 0,
-						thing: true,
-						someOtherProperty: false,
-						extremelyLongPropertyName: {
-							enabled: true,
-							bumpscosity: Math.random(),
-						},
-						yetAnotherProperty: [Math.random(), false, "Hi!"],
-					}).filter(() => Math.random() < 0.5),
-				)),
+				storage: JSON.stringify(
+					Object.fromEntries(
+						Object.entries({
+							someValue: 0,
+							thing: true,
+							someOtherProperty: false,
+							extremelyLongPropertyName: {
+								enabled: true,
+								bumpscosity: Math.random(),
+							},
+							yetAnotherProperty: [Math.random(), false, "Hi!"],
+						}).filter(() => Math.random() < 0.5),
+					),
+				),
 			},
 		]),
 	),
 	themes: Object.fromEntries(
-		new Array(5).fill(0).map(() => [
+		new Array(10).fill(0).map(() => [
 			`${baseThemeRoot}${crypto.randomUUID()}.json`,
 			{
 				enabled: Math.random() < 0.5,
@@ -37,14 +39,14 @@ const mockData: UserData = {
 	),
 	fonts: {
 		installed: Object.fromEntries(
-			new Array(5)
+			new Array(10)
 				.fill(0)
 				.map(() => [
 					`${baseFontRoot}${crypto.randomUUID()}.json`,
 					{ enabled: Math.random() < 0.5 },
 				]),
 		),
-		custom: new Array(2).fill(0).map(() => ({
+		custom: new Array(1).fill(0).map(() => ({
 			spec: 2,
 			name: crypto.randomUUID(),
 			previewText: new Array(Math.floor(Math.random() * 5) + 1)
@@ -64,4 +66,4 @@ const mockData: UserData = {
 	},
 };
 
-console.log(JSON.stringify(mockData));
+if (require.main === module) console.log(JSON.stringify(mockData, undefined, 4));
