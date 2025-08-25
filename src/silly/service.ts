4c5f0ff058d38fe9580bc2638b394871caba86a7
nexpid <me@nexpid.xyz>
2025-08-25 10:28:29 +0200

    chore(silly): ok.....

--- src/silly/service.ts
+++ src/silly/service.ts
@@ -1,7 +1,7 @@
 function hslToHex(h: number, s: number, l: number) {
 	l /= 100;
 	const a = (s * Math.min(l, 1 - l)) / 100;
-	const f = (n) => {
+	const f = (n: number) => {
 		const k = (n + h / 30) % 12;
 		const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
 		return Math.round(255 * color)
